a1e35ae14dd68b537ca2517500ceb21caf34e398
Tusharbecoding <tusharchopra02122002@gmail.com>
2022-05-05 15:02:38 +0530

    many changes

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <div className="heading">Lifesap Healcare</div>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className = "para">
           Coming Soon!!!
         </p>
        
