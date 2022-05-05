f2130ff583095b07d27c0ac5c70bde749537f47c
Tusharbecoding <tusharchopra02122002@gmail.com>
2022-05-05 14:22:24 +0530

    removed learn react

--- src/App.js
+++ src/App.js
@@ -9,14 +9,7 @@ function App() {
         <p>
           Coming Soon!!!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
